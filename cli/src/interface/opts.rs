f8becfec32264046cd4d21e40d6749e285238e25
delucca <delucca@pm.me>
2021-06-21 14:23:31 -0300

    chore: updates description

--- cli/src/interface/opts.rs
+++ cli/src/interface/opts.rs
@@ -3,7 +3,7 @@ use structopt::StructOpt;
 #[derive(StructOpt, Debug)]
 #[structopt(
   name = "chia-seeder",
-  about = "🌱 Easy to use, blazing fast, and reliable Chia plot manager for your farm"
+  about = "🌱 Easy to use, blazing-fast, and reliable Chia plot manager for your farm"
 )]
 pub struct Global {
   #[structopt(short, long)]
