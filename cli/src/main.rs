10d2deab606c43fb03e2394055d852fecf342842
delucca <delucca@pm.me>
2021-06-21 01:39:06 -0300

    chore: add some TODOs

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -5,4 +5,11 @@ use structopt::StructOpt;
 fn main() {
   let global_opts = interface::opts::Global::from_args();
   println!("{:#?}", global_opts);
+
+  // TODO Write hexagonal DDD ADR
+  // TODO Draw initial tool structure
+  // TODO Write initial tool structure ADR
+  // TODO Draw CLI interface
+  // TODO Write CLI interface ADR
+  // TODO Start coding CLI interface
 }
