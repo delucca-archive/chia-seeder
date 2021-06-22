6afcc5bc79e48ef58a8790ee755995298707166c
delucca <delucca@pm.me>
2021-06-22 01:02:22 -0300

    chore: removes TODO

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -5,8 +5,4 @@ use structopt::StructOpt;
 fn main() {
   let global_opts = interface::opts::Global::from_args();
   println!("{:#?}", global_opts);
-
-  // TODO Draw CLI interface
-  // TODO Write CLI interface ADR
-  // TODO Start coding CLI interface
 }
