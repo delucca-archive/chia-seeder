0c118f7172ddf5b1a7e8110399fdeae6acbbd82e
delucca <delucca@pm.me>
2021-06-22 00:26:59 -0300

    chore(docs): updates TODO

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -6,8 +6,6 @@ fn main() {
   let global_opts = interface::opts::Global::from_args();
   println!("{:#?}", global_opts);
 
-  // TODO Draw initial tool structure
-  // TODO Write initial tool structure ADR
   // TODO Draw CLI interface
   // TODO Write CLI interface ADR
   // TODO Start coding CLI interface
